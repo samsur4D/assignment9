3b9d934591d329e40bb8024009e0274cbbc631b3
samsur4D <patarehan14600@gmail.com>
2024-04-18 12:34:00 +0600

    fifth commit

--- src/Components/HomeCard.jsx
+++ src/Components/HomeCard.jsx
@@ -9,20 +9,38 @@ const HomeCard = () => {
     },[])
     
     return (
-     <div className='grid grid-col'>
-        <h2>{venue.length}</h2>
+     <div className=''>
+        {/* <h2>{venue.length}</h2> */}
+        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
+                {
+                    venue.map((vn)=> ( 
+                        <div key={vn.id} className=" rounded-md shadow-md dark:bg-gray-50 dark:text-gray-800">
+                        <img src={vn.image} alt="" className="object-cover object-center w-full rounded-t-md h-72 dark:bg-gray-500" />
+                        <div className="flex flex-col justify-between p-6 space-y-8">
+                            <div className="space-y-2">
+                                <h2 className="text-3xl font-semibold tracking-wide">{vn.estate_title}</h2>
+                                <p className="dark:text-gray-800">Status : {vn.status}</p>
+                                <p className="dark:text-gray-800">Location : {vn.location}</p>
+                               
+                            </div>
+                            {/* ---------- */}
+                            <button to="/details" href="#_" className="relative px-5 py-3 overflow-hidden font-medium text-gray-600 bg-gray-100 border border-gray-100 rounded-lg shadow-inner group">
+<span className="absolute top-0 left-0 w-0 h-0 transition-all duration-200 border-t-2 border-gray-600 group-hover:w-full ease"></span>
+<span className="absolute bottom-0 right-0 w-0 h-0 transition-all duration-200 border-b-2 border-gray-600 group-hover:w-full ease"></span>
+<span className="absolute top-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease"></span>
+<span className="absolute bottom-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease"></span>
+<span className="absolute inset-0 w-full h-full duration-300 delay-300 bg-gray-900 opacity-0 group-hover:opacity-100"></span>
+<span className="relative transition-colors duration-300 delay-200 group-hover:text-white ease">View Details</span>
+</button>
+                            {/* ------------- */}
+                        </div>
+                         </div>
+                    ) )
+                }
+        </div>
         
                    {/* ----------- */}
-                   <div className="max-w-xs rounded-md shadow-md dark:bg-gray-50 dark:text-gray-800">
-        <img src="https://source.unsplash.com/random/300x300/?2" alt="" className="object-cover object-center w-full rounded-t-md h-72 dark:bg-gray-500" />
-        <div className="flex flex-col justify-between p-6 space-y-8">
-            <div className="space-y-2">
-                <h2 className="text-3xl font-semibold tracking-wide">Donec lectus leo</h2>
-                <p className="dark:text-gray-800">Curabitur luctus erat nunc, sed ullamcorper erat vestibulum eget.</p>
-            </div>
-            <button type="button" className="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md dark:bg-violet-600 dark:text-gray-50">Read more</button>
-        </div>
-         </div>
+        
                    {/* --------- */}
      </div>
     );
